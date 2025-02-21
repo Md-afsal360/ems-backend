fad4e001c57fcf393cf31832b2e7a00eb13e26fa
Md-afsal360 <afsalvalt246@gmail.com>
2025-02-21 10:58:27 +0530

    second-try

---
+++ src/test/java/com/afsal/emc/emc_backend/EmcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afsal.emc.emc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
